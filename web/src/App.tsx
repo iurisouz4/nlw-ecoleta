f0ab249c66212e7bc57129ee32aa6af672664b93
Iuri Souza <iuri.santanna@tvglobo.com.br>
2020-06-04 22:11:26 -0300

    info: basic react concepts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Ecoleta" />
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>
+        Aumentar
+      </button>
+    </div>
+  );
 }
 
 export default App;
