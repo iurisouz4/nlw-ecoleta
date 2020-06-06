223b5ecbb56d427999116b7a0aa2c4451cc4f04d
Iuri Souza <iuri.santanna@tvglobo.com.br>
2020-06-06 00:31:51 -0300

    finish core functionalities

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
         id: item.id,
         title: item.title,
         image: item.image,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.0.106:3333/uploads/${item.image}`,
       };
     });
     return res.json(serializedItems);
